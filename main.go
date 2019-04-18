a4b13654f9e70b92599e8dd03d1dc7870675c713
Kerem Karatal <kerem.karatal@xing.com>
2019-04-18 12:37:15 +0200

    Remove superfluous usage of config in main

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/keremk/challenge-bot/config"
 	"github.com/keremk/challenge-bot/slack"
 )
 
 func main() {
-	c := config.GetChallengeConfig()
-	fmt.Println(c)
-
 	slack.SetupSlackListeners()
 }
